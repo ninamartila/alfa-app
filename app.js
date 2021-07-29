19a78a876ee0588f2f3b09cd41864da412ddee38
FakhrulMRijal <fakhrulmrijal7599@gmail.com>
2021-07-30 04:59:04 +0700

    Alfa Commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const router = require('./routers');
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.use(session({
